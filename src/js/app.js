c923edd9477ba20abee2241ef32ffea1cdf24f75
sheshasai <sheshasai.m@turvo.com>
2019-01-08 03:30:51 +0530

    Create, scan page UI

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,111 @@
 var id = 1;
 var items = [];
 
-var $candidatesResults = $("#candidatesResults");
+var $itemList = $("#item-list");
+var $itemTimeline = $("#item-timeline");
 
 function showItem(item) {
-    var candidateTemplate = "<tr><td>" + item.name + "</td><td>" + item.address + "</td></tr>";
-    $candidatesResults.append(candidateTemplate);
+
+    const qrCodeId = `qr-code-${item.address}`;
+    const text = item.address;
+    //var itemDate = new Date(parseInt(item.date.toString())*1000);
+    const date =  moment.unix(parseInt(item.date.toString())).format('YYYY-MM-DD HH:mm:ss');// Date.parse(itemDate).toString('yyyy-MM-dd H:i:s')
+
+   var itemSnip = `<div class="col-sm-2 item-card">
+                            <div class="card">
+                                <div class="card-img-qr" id="qr-code-${item.address}"></div>
+                                <div class="card-body">
+                                    <h4 class="card-title">${item.name}</h4>
+                                    <p class="card-text">${item.address}</p>
+                                     <p class="card-text">${date}</p>
+                                    
+                                </div>
+                            </div>
+                        </div>`;
+    console.log(itemSnip);
+    $itemList.append(itemSnip);
+    console.log(qrCodeId);
+
+    // $itemList.append(itemSnip,function(index,html){
+    //     console.log(id);
+    //     new QRCode(document.getElementById(id), {
+    //                       text,
+    //                       width: 220,
+    //                       height: 220,
+    //                       colorDark: "#000000",
+    //                       colorLight: "#ffffff",
+    //                       correctLevel: QRCode.CorrectLevel.H
+    //                   });
+    // })
+     new QRCode(document.getElementById(qrCodeId), {
+                          text,
+                          width: 220,
+                          height: 220,
+                          colorDark: "#000000",
+                          colorLight: "#ffffff",
+                          correctLevel: QRCode.CorrectLevel.H
+                      });
+    
+}
+
+
+function createItemCard(item){
+
+    var $itemRes = $("#item-res");
+    $itemRes.empty();
+    const date =  moment.unix(parseInt(item.date.toString())).format('YYYY-MM-DD HH:mm:ss');// Date.parse(itemDate).toString('yyyy-MM-dd H:i:s')
+
+     const itemHtml = `<div class="card-body">
+                                    <h5 class="card-title">${item.name}</h5>
+
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">
+                                        <table>
+                                            <tr>
+                                                <td>Address</td>
+                                                <td>${item.address}</td>
+                                            </tr>
+
+                                        </table>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <table>
+                                            <tr>
+                                                <td>Date</td>
+                                                <td>${date}</td>
+                                            </tr>
+
+                                        </table>
+                                    </li>
+                                     <li class="list-group-item">
+                                        <table>
+                                            <tr>
+                                                <td>Manufacturer</td>
+                                                <td>${item.manufacturer}</td>
+                                            </tr>
+
+                                        </table>
+                                    </li>
+
+                                </ul>`;
+
+   $itemRes.append(itemHtml);
+}
+
+function createTimeLine(trace){
+    console.log(trace);
+     date =  moment.unix(parseInt(trace.date.toString())).format('YYYY-MM-DD HH:mm:ss');
+    const traceHtml = `<div class="entry">
+                                            <div class="title">
+                                                <h3>${date}</h3>
+                                            </div>
+                                            <div class="body">
+                                                <p>${trace.owner}</p>
+
+                                            </div>
+                                        </div>`;
+    $itemTimeline.append(traceHtml);                                    
 }
 
 App = {
@@ -41,6 +141,7 @@ App = {
                     .then(function (items) {
                         var accountItems = items[1];
                         console.log(accountItems);
+                        $itemList.empty();
                         $.getJSON("Item.json", function (item) {
                             // Instantiate a new truffle contract from the artifact
                             App.contracts.Item = TruffleContract(item);
@@ -49,14 +150,32 @@ App = {
 
                             for (var i = 0; i < accountItems.length; i++) {
                                 var address = accountItems[i];
+                                var index = i;
                                 App.contracts.Item.at(address).then(function (instance) {
-                                    instance.name().then(function (name) {
-                                        console.log(name);
+
+                                    var namePromise = instance.name();
+                                    var datePromise = namePromise.then(function(name) {
+                                        return instance.createdDate();
+                                    });
+                                    Promise.all([namePromise, datePromise]).then(function([name, date]) {
                                         showItem({
                                             name: name,
-                                            address: instance.address
+                                            address: instance.address,
+                                            date,
+                                            
                                         });
                                     });
+
+
+                                    // instance.name().then(function (name) {
+                                    //     console.log(name);
+                                    //     showItem({
+                                    //         name: name,
+                                    //         address: instance.address
+                                            
+                                            
+                                    //     });
+                                    // });
                                 });
                             }
 
@@ -99,16 +218,38 @@ App = {
         });
     },
 
-    searchItem: function () {
-        $candidatesResults.empty();
-        var address = $("#searchItem").val();
+    searchItem: function (address) {
+       
         App.contracts.Item.at(address).then(function (instance) {
-            instance.name().then(function (i) {
-                showItem({
-                    id: "Something",
-                    name: i,
-                    address: address
+
+            var namePromise = instance.name();
+            var datePromise = namePromise.then(function(name) {
+                return instance.createdDate();
+            });
+            var manufacturerPromise = instance.manufacturer();
+            var ownerShipTracePromise = instance.getOwnershipTrace();                        
+            Promise.all([namePromise, datePromise, manufacturerPromise, ownerShipTracePromise]).then(function([name, date, manufacturer, ownerShipTrace]) {
+                // console.log(ownerShipTrace.toString());
+                console.log(ownerShipTrace);
+                 let traceParts = ownerShipTrace.toString().split(',');
+                createItemCard({
+                    name,
+                    date,
+                    manufacturer,
+                    address: instance.address
                 });
+
+
+                let count = parseInt(ownerShipTrace[0]);
+                let owners = ownerShipTrace[1];
+                let dates = ownerShipTrace[2];
+                let modes = ownerShipTrace[3];
+                let comments = ownerShipTrace[4].split('###');
+                $itemTimeline.empty();
+                for(var i=0;i< count;i++){
+                    createTimeLine({owner: owners[i], date: dates[i], mode: modes[i], comment: comments[i]})
+                }
+
             });
         });
 
@@ -129,5 +270,27 @@ App = {
 $(function () {
     $(window).load(function () {
         App.init();
+        
     });
+    $(document).ready(function () {
+        $('.fixed-action-btn').floatingActionButton();
+    });
+
+     let scanner = new Instascan.Scanner({
+            video: document.getElementById('preview')
+        });
+        scanner.addListener('scan', function (content, image) {
+            console.log(content);
+           App.searchItem(content);
+        });
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (e) {
+            console.error(e);
+        });
+
 });
